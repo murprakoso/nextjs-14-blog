e2b130f2e190558d2288127de0214f466c286e82
murprakoso <prakoso.clan04@gmail.com>
2024-02-28 02:12:34 +0700

    setup layouting

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="w-full relative flex item-center justify-between max-w-2xl mx-auto px-4 py-5">
+    <nav className="w-full relative flex item-center justify-between max-w-2xl mx-auto py-5">
       <Link href="/" className="font-bold text-3xl">
         Muru<span className="text-blue-500">Blog</span>
       </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navbar from "./components/Navbar";
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Hello from index page</h1>
     </div>
   );
