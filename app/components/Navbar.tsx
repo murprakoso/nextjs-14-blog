c15c8a10f291920a7102372d24475eec24471048
murprakoso <prakoso.clan04@gmail.com>
2024-02-28 02:06:41 +0700

    setup schadcn ui, dark mode theme

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full relative flex item-center justify-between max-w-2xl mx-auto px-4 py-5">
+      <Link href="/" className="font-bold text-3xl">
+        Muru<span className="text-blue-500">Blog</span>
+      </Link>
+      <ModeToggle />
+    </nav>
+  );
+}
